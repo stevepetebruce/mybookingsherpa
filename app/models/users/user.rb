3daafcf05f05410dc530914ac481aaebcc08b522
Alan Donohoe <alandonohoe@hotmail.com>
2018-09-24 18:44:54 +0100

    Fix users name and phone_number regex

--- app/models/users/user.rb
+++ app/models/users/user.rb
@@ -1,8 +1,9 @@
 # Super class for all users: guests, guides.
 class User < ApplicationRecord
   validates :email, format: /\A\w+@\w+\.{1}[a-zA-Z]{2,}\z/, presence: true, uniqueness: true
-  validates :name, format: /\A[\sa-zA-Z0-9_.\-]+\z/, allow_blank: true
-  validates :phone_number, format: /\A[0-9+.x()\-]{7,}\z/, allow_blank: true
+  validates :name, format: /\A[\sa-zA-Z0-9_.'\-]+\z/, allow_blank: true
+  validates :phone_number, format: /\A[0-9+.x()\-\s]{7,}\z/, allow_blank: true
   validates :type, inclusion: { in: %w(Guide Guest),
     message: "%{value} is not a valid type of user" }, presence: true
+
 end
