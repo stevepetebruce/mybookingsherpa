ead26896de7c987bbec7487ebde1b24a176fc8d7
Alan Donohoe <alandonohoe@hotmail.com>
2018-09-24 19:03:58 +0100

    Fix FactoryBot deprecation warnings

--- spec/factories/trip.rb
+++ spec/factories/trip.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :trip do
-    name Faker::Name.name
+    name { Faker::Name.name }
   end
 end
--- spec/factories/users/guide.rb
+++ spec/factories/users/guide.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :guide, parent: :user, class: 'Guide' do
-    address Faker::Address.full_address
-    email Faker::Internet.email
-    name Faker::Name.name
-    phone_number Faker::PhoneNumber.cell_phone
-    type 'Guide'
+    address { Faker::Address.full_address }
+    email { Faker::Internet.email }
+    name { Faker::Name.name }
+    phone_number { Faker::PhoneNumber.cell_phone }
+    type { 'Guide' }
   end
 end
