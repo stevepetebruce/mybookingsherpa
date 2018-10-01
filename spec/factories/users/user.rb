e7cbb927401f793749748fcfa2e1180aa16f2af5
Alan Donohoe <alandonohoe@hotmail.com>
2018-10-01 18:40:44 +0100

    Update specs to handle new Devise email requirements.
    This includes the action of Devise downcasing emails before comparing
    and that all users need to have a password to be valid now.

--- spec/factories/users/user.rb
+++ spec/factories/users/user.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     address { Faker::Address.full_address }
     email { Faker::Internet.email }
     name { Faker::Name.name }
+    password { Faker::Internet.password }
     phone_number { Faker::PhoneNumber.cell_phone }
     type { %w(Guest Guide).sample }
   end
