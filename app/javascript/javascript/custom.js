029685fd6ad1e27912a27008b19136feb8f59f8b
Alan Donohoe <alandonohoe@hotmail.com>
2018-10-28 13:11:04 +0000

    Add custom.js
    Part of the work to incorporate Stripe and Stripe.js.
    We need to start adding our own custom JS.
    Subsequent work will break all JS into more nuanced files and only
    load specific ones as required. Esp. the JS loading on the payment page
    for security reasons.
    
    Even futher work would be to add Stimulus JS as and when required.

---
+++ app/javascript/javascript/custom.js
@@ -0,0 +1,3 @@
+// TODO: break this down into more nuanced files and load with webpack
+// Keep all Stripe related JS in one file, and only load that file on sensitive pages.
+// Eventually we can move most/all JS into Stimulus JS, ref: https://stimulusjs.org/
