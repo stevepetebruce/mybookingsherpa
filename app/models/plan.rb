fcfee5702dc29c56b6ba327b2cc9619f9e96fa10
Alan Donohoe <alandonohoe@hotmail.com>
2018-11-03 15:18:45 +0000

    Create Plan model.
    Part of the work to allow guests to make a payment to a guide.
    
    A guide's organisation will be associated with a plan (via a
    subscription), which will be used to apply an application/platform
    fee to each charge, which will paid to our account.
    
    Subsequent work will add subscription model, the join between plans
    and organisations.

---
+++ app/models/plan.rb
@@ -0,0 +1,13 @@
+class Plan < ApplicationRecord
+  enum charge_type: [ :flat_fee, :percentage ]
+
+  validates :name, presence: true
+  validate :charge_amount_present
+
+  private
+
+  def charge_amount_present
+    errors.add(:flat_fee_amount, "can't be blank") if flat_fee? && flat_fee_amount.blank?
+    errors.add(:percentage_amount, "can't be blank") if percentage? && percentage_amount.blank?
+  end
+end
---
+++ spec/models/plan_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Plan, type: :model do
+  # describe 'associations' do
+  # end
+
+  describe 'validations' do
+    context 'name' do
+      it { should validate_presence_of(:name)  }
+    end
+    context 'flat_fee_amount' do
+      subject { FactoryBot.build(:plan, charge_type: :flat_fee) }
+      it { should validate_presence_of(:flat_fee_amount)  }
+    end
+    context 'percentage_amount' do
+      subject { FactoryBot.build(:plan, charge_type: :percentage) }
+      it { should validate_presence_of(:percentage_amount) }
+    end
+  end
+
+  it { should define_enum_for(:charge_type).with([:flat_fee, :percentage ]) }
+end
