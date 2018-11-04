8c5890489f1d4d726dc9c5ebe4400ba120823aa6
stevepetebruce <stevepetebruce@hotmail.com>
2018-11-04 13:24:13 +0000

    Stripe form Improved stripe form styling, connected js to form and amended javascript linting styles in custon js file.

--- app/javascript/javascript/custom.js
+++ app/javascript/javascript/custom.js
@@ -3,61 +3,72 @@
 // Eventually we can move most/all JS into Stimulus JS, ref: https://stimulusjs.org/
 
 (function() {
-  'use strict';
+  "use strict";
 
-  const stripe = Stripe('STRIPE_PUBLISHABLE_KEY');
-  const elements = stripe.elements();
+  document.addEventListener("DOMContentLoaded", function(){
+    const stripe = Stripe("STRIPE_PUBLISHABLE_KEY");
+    const elements = stripe.elements();
+    const stripeTokenHandler = (token) => {
+      // Insert the token ID into the form so it gets submitted to the server
+      const form = document.getElementById("payment-form");
+      const hiddenInput = document.createElement("input");
+      hiddenInput.setAttribute("type", "hidden");
+      hiddenInput.setAttribute("name", "stripeToken");
+      hiddenInput.setAttribute("value", token.id);
+      form.appendChild(hiddenInput);
+      // Submit the form
+      form.submit();
+    };
 
-  // Custom styling can be passed to options when creating an Element.
-  const style = {
-    base: {
-      color: '#32325d',
-      lineHeight: '24px',
-      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-      fontSmoothing: 'antialiased',
-      fontSize: '18px',
-      '::placeholder': {
-        color: '#aab7c4'
+    // Custom styling can be passed to options when creating an Element.
+    const style = {
+      base: {
+        color: "#32325d",
+        lineHeight: "22px",
+        fontFamily: "'Helvetica Neue', Helvetica, sans-serif",
+        fontSmoothing: "antialiased",
+        fontSize: "17px",
+        "::placeholder": {
+          color: "#aab7c4"
+        }
+        },
+        invalid: {
+          color: "#fa755a",
+          iconColor: "#fa755a"
+        }
+    };
+
+    // Create an instance of the card Element.
+    const card = elements.create("card", {style});
+
+    // Add an instance of the card Element into the `card-element` <div>.
+    card.mount("#card-element");
+
+    card.addEventListener("change", function(event) {
+      var displayError = document.getElementById("card-errors");
+      if (event.error) {
+        displayError.textContent = event.error.message;
+      } else {
+        displayError.textContent = "";
       }
-      },
-      invalid: {
-        color: '#fa755a',
-        iconColor: '#fa755a'
+    });
+
+    // Create a token or display an error when the form is submitted.
+    const form = document.getElementById("payment-form");
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      const {token, error} = await stripe.createToken(card);
+
+      if (error) {
+        // Inform the customer that there was an error.
+        const errorElement = document.getElementById("card-errors");
+        errorElement.textContent = error.message;
+      } else {
+        // Send the token to your server.
+        stripeTokenHandler(token);
       }
-  };
+    });
 
-  // Create an instance of the card Element.
-  const card = elements.create('card', {style});
-
-  // Add an instance of the card Element into the `card-element` <div>.
-  card.mount('#card-element');
-
-
-  card.addEventListener('change', function(event) {
-    var displayError = document.getElementById('card-errors');
-    if (event.error) {
-      displayError.textContent = event.error.message;
-    } else {
-      displayError.textContent = '';
-    }
   });
-
-  // Create a token or display an error when the form is submitted.
-  const form = document.getElementById('payment-form');
-  form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const {token, error} = await stripe.createToken(card);
-
-    if (error) {
-      // Inform the customer that there was an error.
-      const errorElement = document.getElementById('card-errors');
-      errorElement.textContent = error.message;
-    } else {
-      console.log(token);
-      // Send the token to your server.
-      stripeTokenHandler(token);
-    }
-  });
-
-})();
+}());
