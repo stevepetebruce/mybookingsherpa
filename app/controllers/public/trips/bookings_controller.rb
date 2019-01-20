16ff52ebb43ca66309e4859f079536127d84aa54
Alan Donohoe <alandonohoe@hotmail.com>
2019-01-20 14:57:22 +0000

    Refactor all public resources to only access bookings (rather than guests).
    Part of the work to allow a publicly accessible booking URL.
    
    Unauthenticated users will be able to access a public/trips/:trip_id/bookings/new
    page, enter their card details and create a new booking on a trip.
    
    This commit contains the final routes and controller (and associated specs)
    changes that implement this.

--- app/controllers/public/trips/bookings_controller.rb
+++ app/controllers/public/trips/bookings_controller.rb
@@ -1,33 +1,34 @@
 module Public
   module Trips
     class BookingsController < ApplicationController
-      skip_before_action :authenticate_guide!
-      before_action :set_booking, only: [:show, :edit, :update]
-      before_action :set_trip, only: [:create, :new]
-      before_action :set_guest, only: [:create]
+      TIMEOUT_WINDOW_MINUTES = 30
+      skip_before_action :authenticate_guide! # / :authenticate_guest! ?
+      before_action :set_booking, only: %i[edit update show]
+      before_action :check_timeout, only: %i[edit update show]
+      before_action :set_trip, only: %i[create new]
 
-      # GET /bookings/1
-      def show
-      end
+      layout 'public'
 
       # GET /bookings/new
       def new
         @booking = Booking.new
       end
 
-      # GET /bookings/1/edit
-      def edit
-      end
-
       # POST /bookings
       def create
+        @guest = Guest.find_or_create_by!(email: booking_params[:email])
         @booking = @trip.bookings.new(booking_params.merge(guest: @guest))
 
-        if @booking.save
-          redirect_to edit_public_guest_path(@guest), notice: 'Booking was successfully created.'
+        if @booking.save && create_charge
+          redirect_to edit_public_booking_path(@booking), notice: 'Booking was successfully created.'
         else
           render :new
         end
+      end
+
+      # GET /bookings/1/edit
+      def edit
+        @trip = @booking.trip
       end
 
       # PATCH/PUT /bookings/1
@@ -40,26 +41,36 @@ module Public
       end
 
       private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_booking
-        @booking = Booking.find(params[:id])
+
+      def booking_params
+        params.require(:booking).permit(:email)
       end
 
-      def set_guest
-        @guest = Guest.find_or_create_by!(email: guest_params[:email])
+      def check_timeout
+        return if newly_created?
+
+        @booking.errors.add(:base, :timeout, message: 'timed out please contact support')
+        redirect_back(fallback_location: new_public_trip_booking_path(@booking.trip))
+      end
+
+      def create_charge
+        Bookings::Payment.new(@booking, stripe_token).charge
+      end
+
+      def newly_created?
+        @booking.created_at > TIMEOUT_WINDOW_MINUTES.minutes.ago
+      end
+
+      def set_booking
+        @booking = Booking.find(params[:id])
       end
 
       def set_trip
         @trip = Trip.find(params[:trip_id])
       end
 
-      # Only allow a trusted parameter "white list" through.
-      def booking_params
-        params.require(:booking).permit()
-      end
-
-      def guest_params
-        params.require(:booking).permit(:email)
+      def stripe_token
+        params[:stripeToken]
       end
     end
   end
--- spec/requests/public/trips/bookings_controller_spec.rb
+++ spec/requests/public/trips/bookings_controller_spec.rb
@@ -1,107 +1,175 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Public::Trips::BookingsController', type: :request do
-  describe '#show GET /public/bookings/:id' do
-    let(:booking) { FactoryBot.create(:booking)}
-
-    def do_request(url: "/public/bookings/#{booking.id}", params: {})
-      get url, params: params
-    end
-
-    it 'should successfully render' do
-      do_request
-
-      expect(response).to be_successful
-    end
-  end
-
-  describe '#new GET /public/bookings/new' do
+RSpec.describe "Public::Trips::BookingsController", type: :request do
+  describe "#new GET /public/bookings/new" do
     let(:trip) { FactoryBot.create(:trip) }
 
     def do_request(url: "/public/trips/#{trip.id}/bookings/new", params: {})
       get url, params: params
     end
 
-    it 'should successfully render' do
+    it "should successfully render" do
       do_request
 
       expect(response).to be_successful
     end
   end
 
-  describe '#edit GET /bookings/:id/edit' do
-    let(:booking) { FactoryBot.create(:booking)}
-
-    def do_request(url: "/public/bookings/#{booking.id}/edit", params: {})
-      get url, params: params
-    end
-
-    it 'should successfully render' do
-      do_request
-
-      expect(response).to be_successful
-    end
-  end
-
-  describe '#create POST /public/trips/:trip_id/bookings' do
+  describe "#create POST /public/trips/:trip_id/bookings" do
     let(:booking) { Booking.last }
     let!(:email) { Faker::Internet.email }
-    let(:params) { { booking: { email: email } } }
-    let(:trip)  { FactoryBot.create(:trip) }
+    let!(:trip) { FactoryBot.create(:trip) }
+    let(:params) do
+      {
+        booking: { email: email },
+        stripeToken: "tok_#{Faker::Crypto.md5}"
+      }
+    end
+
+    before do
+      stub_request(:post, "https://api.stripe.com/v1/charges")
+        .to_return(status: 200,
+                   body: response_body,
+                   headers: {})
+    end
 
     def do_request(url: "/public/trips/#{trip.id}/bookings", params: {})
       post url, params: params
     end
 
-    context 'a guest who does not yet exist' do
-      let(:guest) { Guest.last }
+    context "valid and successful" do
+      let(:response_body) do
+        "#{file_fixture("stripe_api/successful_charge.json").read}"
+      end
 
-      it 'should create the booking and the guest' do
-        expect { do_request(params: params) }.to change { Guest.count }.by 1
+      context "a guest who does not yet exist" do
+        let(:booking) { Booking.last }
+        let(:guest) { Guest.last }
 
-        expect(trip.reload.bookings).to include booking
-        expect(booking.guest).to eq guest
-        expect(trip.guests).to include guest
+        it "should create the booking and the guest" do
+          do_request(params: params)
 
-        expect(response.code).to eq '302'
-        expect(response).to redirect_to(edit_public_guest_path(guest))
+          expect(Guest.count).to eq 1
+          expect(Booking.count).to eq 1
+
+          expect(trip.reload.bookings).to include booking
+          expect(booking.guest).to eq guest
+          expect(trip.guests).to include guest
+
+          expect(response.code).to eq "302"
+          expect(response).to redirect_to(edit_public_booking_path(booking))
+        end
+      end
+
+      context "a guest who does already exist (via their email address)" do
+        let!(:guest) { FactoryBot.create(:guest, email: email) }
+
+        it "should create the booking but not the guest" do
+          expect { do_request(params: params) }.not_to change { Guest.count }
+
+          expect(trip.reload.bookings).to include booking
+          expect(booking.guest).to eq guest
+          expect(trip.guests).to include guest
+
+          expect(response.code).to eq "302"
+          expect(response).to redirect_to(edit_public_booking_path(booking))
+        end
       end
     end
 
-    context 'a guest who does already exist (their email address)' do
-      let!(:guest) { FactoryBot.create(:guest, email: email) }
+    context "unsuccesful" do
+      # TODO: Stripe / Card issue, invalid email address, ???
+    end
+  end
 
-      it 'should create the booking but not the guest' do
-        expect { do_request(params: params) }.not_to change { Guest.count }
+  describe "#edit GET /public/bookings/:id/edit" do
+    def do_request(url: "/public/bookings/#{booking.id}/edit", params: {})
+      get url, params: params
+    end
 
-        expect(trip.reload.bookings).to include booking
-        expect(booking.guest).to eq guest
-        expect(trip.guests).to include guest
+    context "within timeout window of booking being created" do
+      let(:booking) { FactoryBot.create(:booking, created_at: 10.minutes.ago) }
 
-        expect(response.code).to eq '302'
-        expect(response).to redirect_to(edit_public_guest_path(guest))
+      it "should successfully render" do
+        do_request
+
+        expect(response).to be_successful
+      end
+    end
+
+    context "timeout window of booking creation has expired" do
+      let(:booking) { FactoryBot.create(:booking, created_at: 40.minutes.ago) }
+
+      it "should redirect to the public trip new booking path" do
+        do_request
+
+        expect(response.code).to eq "302"
+        expect(response).to redirect_to(new_public_trip_booking_path(booking.trip))
       end
     end
   end
 
-  describe '#update PATCH /bookings/:id' do
-    let(:booking) { FactoryBot.create(:booking) }
-    # TODO: Replace this, we want to only be able to change status in the backend
-    let(:params) { { booking: { status: :complete } } }
+  describe "#update PATCH /public/bookings/:id/update" do
+    let!(:email) { Faker::Internet.email }
+    let(:params) { { booking: { email: email } } }
 
     def do_request(url: "/public/bookings/#{booking.id}", params: {})
       patch url, params: params
     end
 
-    it 'should update the booking' do
-      do_request(params: params)
+    context "within timeout window of booking being created" do
+      let(:booking) { FactoryBot.create(:booking, created_at: 10.minutes.ago) }
 
-      expect(response.code).to eq '302'
-      expect(response).to redirect_to(public_booking_path(booking))
+      it "should update the booking" do
+        do_request(params: params)
+
+        expect(response.code).to eq "302"
+        expect(response).to redirect_to(public_booking_path(booking))
+
+        expect(booking.reload.email).to eq email
+      end
+    end
+
+    context "timeout window of booking creation has expired" do
+      let(:booking) { FactoryBot.create(:booking, created_at: 40.minutes.ago) }
+
+      it "should redirect to the public trip new booking path" do
+        do_request
+
+        expect(response.code).to eq "302"
+        expect(response).to redirect_to(new_public_trip_booking_path(booking.trip))
+      end
     end
   end
 
-  # TODO: Both of these cases (below) will be for non-public / secure area of app, not public.
-  # TODO: index: will show all of a guests or guides bookings...
-  # TODO: destroy, a guest or guide might want to manually delete a booking... soft delete?
+  describe "#show GET /public/bookings/:id/" do
+    def do_request(url: "/public/bookings/#{booking.id}", params: {})
+      get url, params: params
+    end
+
+    context "within timeout window of booking being created" do
+      let(:booking) { FactoryBot.create(:booking, created_at: 10.minutes.ago) }
+
+      it "should successfully render" do
+        do_request
+
+        expect(response).to be_successful
+      end
+    end
+
+    context "timeout window of booking creation has expired" do
+      let(:booking) { FactoryBot.create(:booking, created_at: 40.minutes.ago) }
+
+      def do_request(url: "/public/bookings/#{booking.id}", params: {})
+        get url, params: params
+      end
+
+      it "should redirect to the public trip new booking path" do
+        do_request
+
+        expect(response.code).to eq "302"
+        expect(response).to redirect_to(new_public_trip_booking_path(booking.trip))
+      end
+    end
+  end
 end
