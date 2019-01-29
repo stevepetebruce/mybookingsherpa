116bd9125fbd0f009a9425d4a53e79e8cb7bd6f9
Alan Donohoe <alandonohoe@hotmail.com>
2019-01-29 04:06:17 +0000

    Tighten up GuestController actions and add sign_in / sign_out spec helpers.
    This work reduces the actions in the GuestController to those we have to have
    to increase security and prevent any destructive behaviour.
    
    Part of the work separating the guest and guide routes.
    
    Calls the Devise authentication method authenticate_guest! on all guest
    routes, so a guest has to log in to visit their data.
    
    Follow up work will add a one-time-only token-based log in URL, which
    will be emailed to the email address in every new booking. Which, when
    clicked creates a new guest (from the booking data).

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,6 +61,8 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Devise::Test::IntegrationHelpers, type: :request
+
+  config.include RequestSpecHelper, type: :request
 end
 
 Shoulda::Matchers.configure do |config|
