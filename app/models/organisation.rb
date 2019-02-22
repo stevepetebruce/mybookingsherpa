a628e08b9449594b55d180be7de9840be58b0c9b
Alan Donohoe <alandonohoe@hotmail.com>
2019-02-22 11:57:15 +0000

    Fix intermittent failing spec caused by invalid organisation subdomain.
    A number of specs were failing intermittently that were relying on the
    organisation factory, which was creating an invalid subdomain for the
    organisation, due to it being too short (< 3 chars).
    
    This was exacerbated by the regex of the subdomain validation that
    actually excluded subdomains that were 3 chars long. This has also been
    fixed in this work.
    
    Subsequent work will fix the intermittently failing specs in other areas.

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -4,7 +4,7 @@ class Organisation < ApplicationRecord
   validates :currency, presence: true
   validates :name, format: /\A[\sa-zA-Z0-9_.'\-]+\z/, allow_blank: false, uniqueness: true
   validates :stripe_account_id, format: /\A[a-zA-Z0-9_\-]{5,50}\z/, allow_blank: true
-  validates :subdomain, format: /\A([a-zA-Z0-9][a-zA-Z0-9_\-]{3,30})\z/, allow_blank: true
+  validates :subdomain, format: /\A([a-zA-Z0-9]{1}[a-zA-Z0-9_\-]{2,30})\z/, allow_blank: true
 
   has_many :organisation_memberships
   has_many :guides, through: :organisation_memberships
