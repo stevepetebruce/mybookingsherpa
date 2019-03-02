9ee16056cc7089d35e92d73da572c44a6ea2d2da
Alan Donohoe <alandonohoe@hotmail.com>
2019-03-02 14:00:36 +0000

    Add set_updatable_fields after_update callback to Guest model.
    Part of the work to copy over data from a booking to an associated
    guest, when the guest clicks on the email confirmation link.
    
    This copies over any FIELD_NAME_booking field values to blank FIELD_NAME
    fields.
    
    This ensures that the guest will have up to date data from their most recent
    booking, if they haven't had any data set previously.
    
    Subsequent work will copy over any FIELD_NAME_override data making sure that
    this data is given precedence over the booking data. This is to make sure that
    guest entered data overrides any data from a public booking.

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,13 @@
 class Guest < ApplicationRecord
+  UPDATABLE_FIELDS = %i[address allergies city country county
+                        date_of_birth dietary_requirements
+                        medical_conditions name
+                        next_of_kin_name next_of_kin_phone_number
+                        phone_number post_code].freeze
+  UPDATABLE_SOURCE_FIELD_POSTFIX = "_booking".freeze
+
+  after_update :set_updatable_fields
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -16,5 +25,25 @@ class Guest < ApplicationRecord
 
   def password_required?
     false
+  end
+
+  private
+
+  def blank_updatable_attributes
+    updatable_attributes.reject { |k, _v| read_attribute(k).present? }
+  end
+
+  def set_updatable_fields
+    return if blank_updatable_attributes.empty?
+
+    update_columns(blank_updatable_attributes)
+  end
+
+  def source_attributes
+    attributes.slice(*UPDATABLE_FIELDS.map { |field| "#{field}#{UPDATABLE_SOURCE_FIELD_POSTFIX}" })
+  end
+
+  def updatable_attributes
+    source_attributes.map { |k, v| [k.gsub(UPDATABLE_SOURCE_FIELD_POSTFIX, ""), v] }.to_h
   end
 end
