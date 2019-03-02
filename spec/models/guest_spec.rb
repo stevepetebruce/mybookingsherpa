b669c98512da11b4f40f1c244835b3e6be430d4d
Alan Donohoe <alandonohoe@hotmail.com>
2019-03-02 14:00:36 +0000

    Add set_updatable_fields after_update callback to Guest model.
    Part of the work to copy over data from a booking to an associated
    guest, when the guest clicks on the email confirmation link.
    
    This copies over any FIELD_NAME_booking field values to blank FIELD_NAME
    fields.
    
    This ensures that the guest will have up to date data from their most recent
    booking, if they haven't had any data set previously.
    
    Subsequent work will copy over any FIELD_NAME_override data making sure that
    this data is given precedence over the booking data. This is to make sure that
    guest entered data overrides any data from a public booking.

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -1,23 +1,33 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Guest, type: :model do
-  describe 'associations' do
+  describe "associations" do
     it { should have_many(:bookings) }
     it { should have_many(:trips).through(:bookings) }
   end
 
-  describe 'validations' do
-    context 'email' do
+  describe "callbacks" do
+    let(:guest) { FactoryBot.create(:guest) }
+
+    it "should call #set_updatable_fields after_update" do
+      expect(guest).to receive(:set_updatable_fields)
+
+      guest.update(email: Faker::Internet.email )
+    end
+  end
+
+  describe "validations" do
+    context "email" do
       it { should allow_value(Faker::Internet.email).for(:email) }
       it { should_not allow_values(Faker::Lorem.word, Faker::PhoneNumber.cell_phone ).for(:email) }
       it { should validate_presence_of(:email) }
       it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
     end
-    context 'name' do
-      it { should_not allow_value('<SQL INJECTION>').for(:name) }
+    context "name" do
+      it { should_not allow_value("<SQL INJECTION>").for(:name) }
       it { should allow_value(Faker::Lorem.word).for(:name) }
     end
-    context 'phone_number' do
+    context "phone_number" do
       it { should allow_value(Faker::PhoneNumber.cell_phone).for(:phone_number) }
       it { should_not allow_value(Faker::Lorem.word).for(:phone_number) }
     end
@@ -25,4 +35,46 @@ RSpec.describe Guest, type: :model do
 
   it { should define_enum_for(:allergies).with(%i[dairy eggs nuts penicillin soya]) }
   it { should define_enum_for(:dietary_requirements).with(%i[other vegan vegetarian]) }
+
+  describe "#set_updatable_fields" do
+    let(:updatable_fields) do
+      %i[address allergies city country county date_of_birth
+         dietary_requirements medical_conditions name next_of_kin_name
+         next_of_kin_phone_number phone_number post_code].freeze
+    end
+
+    context "with no cannonical fields with values" do
+      context "with no booking fields with values" do
+        let(:guest) { FactoryBot.create(:guest) }
+
+        it "should not update the cannonical fields" do
+          expect { guest.send(:set_updatable_fields) }.to_not change { guest.attributes }
+        end
+      end
+
+      context "with booking fields that have values" do
+        let(:guest) do
+          FactoryBot.create(:guest,
+                            :all_booking_fields_complete,
+                            address: nil, name: nil, phone_number: nil)
+        end
+
+        it "should update the cannonical fields from the booking fields" do
+          guest.send(:set_updatable_fields)
+
+          updatable_fields.each do |field|
+            expect(guest.send("#{field}")).to eq guest.send("#{field}_booking")
+          end
+        end
+      end
+    end
+
+    context "with cannonical fields with pre-existing values" do
+      let(:guest) { FactoryBot.create(:guest, :all_updatable_fields_complete) }
+
+      it "should not update the cannonical fields" do
+        expect { guest.send(:set_updatable_fields) }.to_not change { guest.attributes }
+      end
+    end
+  end
 end
