39bc0dad66c93a26719df73b89dae02e2c9cd2ca
Alan Donohoe <alandonohoe@hotmail.com>
2019-03-03 11:28:12 +0000

    Rename Guests::Updater -> Guests::BookingUpdater.
    Part of the work to copy over data from a booking to an associated
    guest, when the guest clicks on the email confirmation link.
    
    The logic for updating a guest from its _booking and _override fields
    became increasingly complex so it made sense to extract it out of the
    Guest model and into its own service model Guests::Updater.
    
    The original class is now renamed Guests::BookingUpdater to allow for
    this.

---
+++ spec/services/guests/booking_updater_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe Guests::BookingUpdater, type: :model do
+  describe "#copy_booking_values" do
+    subject { described_class.new(guest).copy_booking_values(booking) }
+
+    context "a guest with no overriden values set" do
+      let!(:booking) { FactoryBot.build(:booking, :all_fields_complete) }
+      let!(:guest) { FactoryBot.create(:guest) }
+      let(:non_enum_updatable_fields) do
+        updatable_fields.reject do |field|
+          %i[allergies dietary_requirements].include?(field)
+        end
+      end
+      let(:updatable_fields) do
+        %i[address allergies city country county date_of_birth
+           dietary_requirements email medical_conditions name
+           next_of_kin_name next_of_kin_phone_number phone_number
+           post_code].freeze
+      end
+
+      it "should update the guest's 'field'_booking and 'field' values" do
+        subject
+
+        non_enum_updatable_fields.each do |field|
+          expect(guest.send("#{field}_booking")).to eq booking.send(field)
+        end
+      end
+    end
+  end
+end
