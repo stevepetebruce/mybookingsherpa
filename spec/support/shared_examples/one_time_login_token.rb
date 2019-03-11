be2d6d0922cdba5e17befd538f3560a66453908c
Alan Donohoe <alandonohoe@hotmail.com>
2019-03-11 16:26:39 +0000

    Copy over most recent booking's data when guest confirms email.
    We want to copy over any data from the most recent booking to the
    associated blank guest _booking fields when a guest confirms their email
    by clicking on the one time login link in the email we send with each new booking.
    
    Follow up work will fix the pending spec by changing how we deal with the guest's
    fields that are enums. There's an issue with the bulk update process that needs fixing.
    
    Need to convert the columns to strings from integers:
    Ref: https://sipsandbits.com/2018/04/30/using-database-native-enums-with-rails/

--- spec/support/shared_examples/one_time_login_token.rb
+++ spec/support/shared_examples/one_time_login_token.rb
@@ -1,5 +1,13 @@
+# Guest can log in via a one time only log in token
+# sent on new bookings
 RSpec.shared_examples "one_time_login_token" do
   let(:one_time_login_token_param_key) { "#{GuestsController::OBFUSCATED_ONE_TIME_LOGIN_TOKEN_PARAM_NAME}" }
+  let(:onetime_only_login_params) do
+    {
+      email: guest.email,
+      "#{one_time_login_token_param_key}": guest.one_time_login_token
+    }
+  end
 
   context "not signed in" do
     context "no one_time_login_token present" do
@@ -12,24 +20,31 @@ RSpec.shared_examples "one_time_login_token" do
 
     context "invalid one_time_login_token (ie: could of been used before)" do
       it "should redirect the guest to sign in" do
-        do_request(params: params.merge(one_time_login_token: Faker::Lorem.word))
+        do_request(params: params.merge(email: guest.email, one_time_login_token: Faker::Lorem.word))
 
         expect(response).to redirect_to(new_guest_session_path)
       end
     end
 
     context "valid one_time_login_token present" do
+      let!(:booking) { FactoryBot.create(:booking, :all_fields_complete, guest: guest) }
+
       it "should log the guest in" do
-        do_request(params: params.merge(email: guest.email,
-                                        "#{one_time_login_token_param_key}": guest.one_time_login_token))
+        do_request(params: params.merge(onetime_only_login_params))
 
         expect(response).to be_successful
       end
 
       it "should reset the one_time_login_token" do
-        expect do do_request(params: params.merge(email: guest.email,
-                                                  "#{one_time_login_token_param_key}": guest.one_time_login_token))
-        end.to change { guest.reload.one_time_login_token }
+        expect { do_request(params: params.merge(onetime_only_login_params)) }.
+          to change { guest.reload.one_time_login_token }
+      end
+
+      it "should update the guest's booking fields" do
+        pending "Need to fix the enums (ex: dietary_requirements_booking) when copying data"
+        do_request(params: params.merge(onetime_only_login_params))
+
+        expect(guest.reload.address_booking).to eq booking.address
       end
     end
   end
