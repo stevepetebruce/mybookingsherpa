839ff62eb6f8fa8489e972de3acc8862791d658e
Alan Donohoe <alandonohoe@hotmail.com>
2019-03-18 14:09:32 +0000

    Replace hard coded status colour with real booking status in UI.
    Part of the work to replace hard data with real data
    in views in preparation for the first user testing.

--- spec/decorators/guest_decorator_spec.rb
+++ spec/decorators/guest_decorator_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe GuestDecorator, type: :model do
-  let!(:guest) { FactoryBot.build(:guest) }
+  let!(:guest) { FactoryBot.create(:guest) }
 
   describe "#gravatar_url" do
     subject { described_class.new(guest).gravatar_url }
@@ -18,6 +18,31 @@ RSpec.describe GuestDecorator, type: :model do
 
     it "should return expected flag-icon class" do
       expect(["flag-icon-fr", "flag-icon-gb", "flag-icon-us"]).to include(subject)
+    end
+  end
+
+  describe "#status" do
+    subject(:status) { described_class.new(guest).status(trip) }
+
+    let(:trip) { FactoryBot.create(:trip) }
+
+    context "booking is incomplete / yellow" do
+      let!(:booking) { FactoryBot.create(:booking, guest: guest, trip: trip) }
+
+      it "should return the correct flag" do
+        expect(status).to eq "dot-warning"
+      end
+    end
+
+    context "booking is complete / green" do
+      let(:booking) { FactoryBot.create(:booking, :all_fields_complete, guest: guest, trip: trip) }
+      let!(:payment) { FactoryBot.create(:payment, amount: booking.full_cost, booking: booking) }
+
+      before { booking.update(updated_at: Time.zone.now) }
+
+      it "should return the correct flag" do
+        expect(status).to eq "dot-success"
+      end
     end
   end
 end
