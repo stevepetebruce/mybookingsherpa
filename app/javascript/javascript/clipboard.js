cdab4c600f97353f29c5e73436928a312ca11cf5
stevepetebruce <stevepetebruce@hotmail.com>
2019-04-04 23:15:07 +0100

    Style Copy to Clipboard Work to style copy to clipboard on the guide index UI. Styled with icons, responsive design and propagation control on the form and links.

--- app/javascript/javascript/clipboard.js
+++ app/javascript/javascript/clipboard.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   const clipboardButtons = document.querySelectorAll('[data-clipboard-btn]');
+  const clipboardInputForms = document.querySelectorAll('[data-clipboard-source]');
   
   function copyToClipboard(e) {
     const clipboardId = e.srcElement.dataset.clipboardBtn;
     const clipboardInput = document.querySelector(`input[data-clipboard-source="${clipboardId}"]`);
-    console.log(e);
     if(clipboardInput) {
       clipboardInput.select();
       document.execCommand("copy");
@@ -13,4 +13,5 @@ document.addEventListener("DOMContentLoaded", function() {
     e.stopPropagation();
   }
   clipboardButtons.forEach(clipboardButton => clipboardButton.addEventListener("click", copyToClipboard));
+  clipboardInputForms.forEach(clipboardInputForm => clipboardInputForm.addEventListener("click", event => event.stopPropagation()));
 });
