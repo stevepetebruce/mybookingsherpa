2b180cb12eeec009316f8d47babd5d312dadb108
stevepetebruce <stevepetebruce@hotmail.com>
2019-04-11 11:32:12 +0100

    Remove error in the console Work to fix pages without the Select country input throwing an error, stating #booking_country could not be found.

--- app/javascript/javascript/countrySelect.js
+++ app/javascript/javascript/countrySelect.js
@@ -1,5 +1,7 @@
 (async () => {
   const result = await fetch("//freegeoip.app/json/");
   const json = await result.json();
-  document.querySelector("#booking_country").value = json.country_code;
+  const selectCountry = document.querySelector("#booking_country");
+  if (!selectCountry) return;
+  selectCountry.value = json.country_code;
 })();
