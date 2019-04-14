a1b07635d94bb23a9dd562e3ffba3b086838ab14
Alan Donohoe <alandonohoe@hotmail.com>
2019-04-14 10:24:19 +0100

    Redesign the copy to clipboard new booking link.
    This work replaces the input element showing start of the new public
    booking link with a small text element showing the end of the URL.
    
    It wasn't clear what the URL was when you could only see, for ex:
    http://localhost/...
    
    This provides more context to the guide as to what that link actually is.
    
    Follow up work will use slugs using part of the trip name to reference
    trips in the new booking URL, so this link will be even clearer that
    we are dealing with a trip_name/bookings/new URL.

--- app/javascript/javascript/clipboard.js
+++ app/javascript/javascript/clipboard.js
@@ -3,12 +3,27 @@ document.addEventListener("DOMContentLoaded", function() {
   const clipboardButtons = document.querySelectorAll('[data-clipboard-btn]');
   const clipboardInputForms = document.querySelectorAll('[data-clipboard-source]');
 
+  function selectText(clipboardInput) {
+    if (document.body.createTextRange) {
+      const range = document.body.createTextRange();
+      range.moveToElementText(clipboardInput);
+      range.select();
+    } else if (window.getSelection) {
+      const selection = window.getSelection();
+      const range = document.createRange();
+      range.selectNodeContents(clipboardInput);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  }
+
   function copyToClipboard(e) {
     const clipboardId = e.srcElement.dataset.clipboardBtn;
-    const clipboardInput = document.querySelector(`input[data-clipboard-source="${clipboardId}"]`);
+    const clipboardInput = document.querySelector(`small[data-clipboard-source="${clipboardId}"]`);
     const clipboardMessage = document.querySelector(`span[data-clipboard-message="${clipboardId}"]`);
+
     if(clipboardInput) {
-      clipboardInput.select();
+      selectText(clipboardInput);
       document.execCommand("copy");
       clipboardMessage.classList.add("copied");
       setTimeout(() => clipboardMessage.classList.remove("copied"), 1000);
