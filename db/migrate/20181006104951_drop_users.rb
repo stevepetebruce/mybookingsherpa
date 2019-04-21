2def31aa92cf45a536edc7001fdf0f2b70672587
Alan Donohoe <alandonohoe@hotmail.com>
2019-04-21 13:53:38 +0100

    Cascade the deletion of the user table to include dependent tables.
    Trying to reset the database on production is proving very difficult.
    
    Running rails db:migrate stalls at this migration with the error:
    
    PG::DependentObjectsStillExist: ERROR:  cannot drop table users
    because other objects depend on it
    DETAIL:  constraint fk_rails_45cc0c7a87 on table trips depends on table users
    
    This will hopefully fix that issue.

--- db/migrate/20181006104951_drop_users.rb
+++ db/migrate/20181006104951_drop_users.rb
@@ -1,6 +1,6 @@
 class DropUsers < ActiveRecord::Migration[5.2]
   def change
-    drop_table :trips_users
-    drop_table :users
+    drop_table :trips_users, force: :cascade
+    drop_table :users, force: :cascade
   end
 end
