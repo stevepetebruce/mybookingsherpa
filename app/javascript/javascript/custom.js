890fb2e6eb3dca2e59bb271e11ddf17ad4cb8862
Alan Donohoe <alandonohoe@hotmail.com>
2019-04-28 10:20:08 +0100

    Check for Stripe card element before running the Stripe JS.
    Part of the work in JS world was trying to get Bootstrap
    custom validation working. It was necessary to make sure there
    was no errors when working out why the validation wasn't working.
    
    Previously the JS was throwing this error:
    Uncaught IntegrationError: The selector you specified (#card-element)
    applies to no DOM elements that are currently on the page.
    Make sure the element exists on the page before calling mount().
    
    This fixes that.

--- app/javascript/javascript/custom.js
+++ app/javascript/javascript/custom.js
@@ -59,36 +59,38 @@
     const card = elements.create("card", {style});
 
     // Add an instance of the card Element into the `card-element` <div>.
-    card.mount("#card-element");
+    if (document.getElementById("card-element") !== null) {
+      card.mount("#card-element");
 
-    card.addEventListener("change", function(event) {
-      var displayError = document.getElementById("card-errors");
-      if (event.error) {
-        displayError.textContent = event.error.message;
-      } else {
-        displayError.textContent = "";
-      }
-    });
-
-    // Create a token or display an error when the form is submitted.
-    const form = document.getElementById("payment-form");
-    form.addEventListener("submit", async (event) => {
-      event.preventDefault();
-
-      const {token, error} = await stripe.createToken(card);
-
-      if (error) {
-        // Inform the customer that there was an error.
-        const errorElement = document.getElementById("card-errors");
-        errorElement.textContent = error.message;
-      } else {
-        // Send the token to your server.
-        if (form.checkValidity() === true) {
-          stripeTokenHandler(token);
+      card.addEventListener("change", function(event) {
+        var displayError = document.getElementById("card-errors");
+        if (event.error) {
+          displayError.textContent = event.error.message;
+        } else {
+          displayError.textContent = "";
         }
-        form.classList.add("was-validated");
-      }
-      document.querySelectorAll("input[type='submit']")[0].disabled = false;
-    });
+      });
+
+      // Create a token or display an error when the form is submitted.
+      const form = document.getElementById("payment-form");
+      form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const {token, error} = await stripe.createToken(card);
+
+        if (error) {
+          // Inform the customer that there was an error.
+          const errorElement = document.getElementById("card-errors");
+          errorElement.textContent = error.message;
+        } else {
+          // Send the token to your server.
+          if (form.checkValidity() === true) {
+            stripeTokenHandler(token);
+          }
+          form.classList.add("was-validated");
+        }
+        document.querySelectorAll("input[type='submit']")[0].disabled = false;
+      });
+    }
   });
 }());
