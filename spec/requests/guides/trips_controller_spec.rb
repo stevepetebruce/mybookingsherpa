stevepetebruce/mybookingsherpa

commit 43202a039c421295b6836a27588e940e740306f3

Simple refactor of Guides::TripsController, based on code standards

app/controllers/guides/trips_controller.rb
@@ -1,7 +1,6 @@
 module Guides
   class TripsController < ApplicationController
     before_action :authenticate_guide!
-    before_action :set_show_past_trips, only: %i[index]
 
     def create
       @trip = current_organisation.trips.new(trip_params)
@@ -14,15 +13,11 @@ module Guides
     end
 
     def index
-      if @show_past_trips
-        @trips = current_guide.trips.past_trips.map do |trip|
-          TripDecorator.new(trip)
-        end
-      else
-        @trips = current_guide.trips.future_trips.map do |trip|
-          TripDecorator.new(trip)
-        end
-      end
+      @trips = if show_past_trips?
+                 current_guide.trips.past_trips.map { |trip| TripDecorator.new(trip) }
+               else
+                 current_guide.trips.future_trips.map { |trip| TripDecorator.new(trip) }
+               end
     end
 
     def new; end
@@ -34,8 +29,8 @@ module Guides
       current_guide.organisation_memberships.owners.first.organisation
     end
 
-    def set_show_past_trips
-      @show_past_trips = params[:past_trips].presence == "true" ? true : false
+    def show_past_trips?
+      params[:past_trips].presence == "true"
     end
 
     def trip_params

commit ff1866d8c82446b610861d01966292cd4f4d36e5

Fix the Past Trips view not showing link to "My (future) Trips".
Previous work refactoring the guides/trips controller
43202a039c421295b6836a27588e940e740306f3
accidentally broke the Past Trips view and resulted in the link
at the bottom of the list of trips was always linking to "My Past Trips".

This work fixes that.

app/controllers/guides/trips_controller.rb
@@ -2,6 +2,7 @@ module Guides
   class TripsController < ApplicationController
     before_action :authenticate_guide!
     before_action :set_trip, only: %i[edit update]
+    before_action :set_show_past_trips, only: %i[index]
 
     def create
       @trip = current_organisation.trips.new(trip_params)
@@ -16,7 +17,7 @@ module Guides
     def edit; end
 
     def index
-      @trips = if show_past_trips?
+      @trips = if @show_past_trips
                  current_guide.trips.past_trips.map { |trip| TripDecorator.new(trip) }
                else
                  current_guide.trips.future_trips.map { |trip| TripDecorator.new(trip) }
@@ -44,8 +45,8 @@ module Guides
       @trip = current_guide.trips.find(params[:id])
     end
 
-    def show_past_trips?
-      params[:past_trips].presence == "true"
+    def set_show_past_trips
+      @show_past_trips = params[:past_trips].presence == "true" ? true : false
     end
 
     def trip_params

spec/requests/guides/trips_controller_spec.rb
@@ -139,6 +139,12 @@ RSpec.describe "Guides::TripsController", type: :request do
 
                 expect(response.body).to_not include(trip.name)
               end
+
+              it "should have a link to past trips" do
+                do_request
+
+                expect(response.body).to include('<a class="btn btn-primary" href="/guides/trips?past_trips=true">My Past Trips</a>')
+              end
             end
 
             context "with past_trips=true" do
@@ -148,6 +154,12 @@ RSpec.describe "Guides::TripsController", type: :request do
                 do_request(params: params)
 
                 expect(response.body).to include(trip.name)
+              end
+
+              it "should have a link to my (future) trips" do
+                do_request(params: params)
+
+                expect(response.body).to include('<a class="btn btn-primary" href="/guides/trips">My Trips</a>')
               end
             end
           end