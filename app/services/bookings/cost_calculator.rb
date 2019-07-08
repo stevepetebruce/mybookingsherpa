f86e5a4880226c147211a2202ac005ee69e0a36c
Alan Donohoe <alandonohoe@hotmail.com>
2019-07-08 12:17:16 +0100

    Allow Bookings::CostCalculator to handle remaining cost due after deposit is paid.
    Part of the work to allow the payment of deposits and then the subsequent automatic
    payment of the remainder at a certain date.
    
    We need to calculate the amount_due for bookings that have paid a deposit and are now due
    to pay the remaining cost.
    
    This required making #total_paid method public in the Bookings::PaymentStatus service model.
    
    This is one part that can be broken out of the main work into digestible pull requests.
    
    More to follow...

--- app/services/bookings/cost_calculator.rb
+++ app/services/bookings/cost_calculator.rb
@@ -12,21 +12,46 @@ module Bookings
 
     private
 
+    def after_full_payment_window?
+      return false if full_payment_window_weeks.blank?
+
+      (@booking.trip_start_date - full_payment_window_weeks.weeks) <
+        Time.zone.now
+    end
+
     def before_full_payment_window?
-      (@booking.trip_start_date - @booking.trip_full_payment_window_weeks.weeks) >
+      return false if full_payment_window_weeks.blank?
+
+      (@booking.trip_start_date - full_payment_window_weeks.weeks) >
         Time.zone.now
     end
 
     def calculate_amount_due
       return @booking.deposit_cost if deposit_due?
+      return full_cost_minus_deposit if remainder_of_full_cost_due?
+      return 0 if nothing_due_now?
 
       @booking.full_cost
     end
 
     def deposit_due?
-      return false if no_full_payment_window? || no_deposit_cost?
+      return false if no_full_payment_window? ||
+                      no_deposit_cost? ||
+                      deposit_paid?
 
       before_full_payment_window?
+    end
+
+    def deposit_paid?
+      total_paid == @booking.deposit_cost
+    end
+
+    def full_cost_minus_deposit
+      @booking.full_cost - @booking.deposit_cost
+    end
+
+    def full_payment_window_weeks
+      @booking.trip_full_payment_window_weeks
     end
 
     def no_deposit_cost?
@@ -34,7 +59,21 @@ module Bookings
     end
 
     def no_full_payment_window?
-      @booking.trip_full_payment_window_weeks.blank?
+      full_payment_window_weeks.blank?
+    end
+
+    def nothing_due_now?
+      !no_deposit_cost? && deposit_paid? && before_full_payment_window?
+    end
+
+    def remainder_of_full_cost_due?
+      return true if deposit_paid? && after_full_payment_window?
+
+      false
+    end
+
+    def total_paid
+      @total_paid ||= Bookings::PaymentStatus.new(@booking).total_paid
     end
   end
 end
--- spec/services/bookings/cost_calculator_spec.rb
+++ spec/services/bookings/cost_calculator_spec.rb
@@ -1,45 +1,121 @@
 require "rails_helper"
-include ActiveSupport::Testing::TimeHelpers
+# 4 possible states:
+
+# 1) Deposit due
+# True, when: before full window period, deposit percentage has been set,
+
+# 2) Full payment due
+# True when: no deposit % has been set
+# True when: deposit % has been set but after full payment period
+
+# 3) Final payment due
+# True when: deposit has been paid, past full payment window
+
+# 4) Nothing due just yet
+# True when: deposit has been paid, before full payment window
 
 RSpec.describe Bookings::CostCalculator, type: :model do
+  include ActiveSupport::Testing::TimeHelpers
+
   let!(:booking) { FactoryBot.create(:booking, trip: trip) }
-  let!(:trip) do 
+  let!(:trip) do
     FactoryBot.create(:trip,
                       currency: :gbp,
                       deposit_percentage: deposit_percentage,
                       end_date: 11.weeks.from_now,
-                      full_cost: rand(5_000_0...10_000_0),
+                      full_cost: Faker::Number.between(5_000_0, 10_000_0),
                       full_payment_window_weeks: full_payment_window_weeks,
-                      start_date: 10.weeks.from_now)
+                      start_date: 8.weeks.from_now)
   end
 
   describe "#amount_due" do
     subject(:amount_due) { described_class.new(booking).amount_due }
 
-    context "before full payment window (in weeks) has elapsed" do
-      let!(:deposit_percentage) { rand(10...50) }
-      let(:full_payment_window_weeks) { 4 }
+    describe "deposit due" do
+      context "deposit_percentage has been set" do
+        let!(:deposit_percentage) { Faker::Number.between(10, 50) }
 
-      it "should be the deposit cost" do
-        travel 4.weeks do
-          expect(amount_due).to eq(trip.deposit_cost)
+        context "before full payment window (in weeks) has elapsed" do
+          let(:full_payment_window_weeks) { 4 }
+
+          it "should be the deposit cost" do
+            travel 3.weeks do
+              expect(amount_due).to eq(trip.deposit_cost)
+            end
+          end
         end
       end
     end
 
-    context "after full payment window (in weeks) has elapsed" do
-      let!(:deposit_percentage) { rand(10...50) }
+    describe "full cost due" do
       let(:full_payment_window_weeks) { 4 }
 
-      it "should be the full trip cost" do
-        travel 7.weeks do
+      context "deposit_percentage has not been set" do
+        let!(:deposit_percentage) { nil }
+
+        it "should be the full cost" do
           expect(amount_due).to eq(trip.full_cost)
         end
       end
+
+      context "deposit_percentage has been set" do
+        let!(:deposit_percentage) { Faker::Number.between(10, 50) }
+
+        context "after full payment window (in weeks) has elapsed" do
+          it "should be the full cost" do
+            travel_to(trip.start_date - 1.week) do
+              expect(amount_due).to eq(trip.full_cost)
+            end
+          end
+        end
+      end
     end
 
+    describe "final payment due" do
+      context "deposit has been paid" do
+        let!(:deposit_payment) do
+          FactoryBot.create(:payment,
+                            amount: trip.deposit_cost,
+                            booking: booking)
+        end
+
+        let!(:deposit_percentage) { Faker::Number.between(10, 50) }
+        let(:full_payment_window_weeks) { 4 }
+
+        context "after full payment window (in weeks) has elapsed" do
+          it "should be the remaining amount due" do
+            travel_to(trip.start_date - 1.week) do
+              expect(amount_due).to eq(trip.full_cost - trip.deposit_cost)
+            end
+          end
+        end
+      end
+    end
+
+    context "no payment due just yet" do
+      context "deposit has been paid" do
+        let!(:deposit_payment) do
+          FactoryBot.create(:payment,
+                            amount: trip.deposit_cost,
+                            booking: booking)
+        end
+
+        let!(:deposit_percentage) { Faker::Number.between(10, 50) }
+        let(:full_payment_window_weeks) { 4 }
+
+        context "before full payment window (in weeks) has elapsed" do
+          it "should be the remaining amount due" do
+            travel_to(trip.start_date - 5.weeks) do
+              expect(amount_due).to eq(0)
+            end
+          end
+        end
+      end
+    end
+
+    # Test misc states
     context "trip has no full payment window set" do
-      let!(:deposit_percentage) { rand(10...50) }
+      let!(:deposit_percentage) { Faker::Number.between(10, 50) }
       let(:full_payment_window_weeks) { nil }
 
       it "should be the full trip cost" do
