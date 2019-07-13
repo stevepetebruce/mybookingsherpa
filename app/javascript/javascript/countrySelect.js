64a1594ef4efe868ad94464a01734c71105423de
stevepetebruce <stevepetebruce@hotmail.com>
2019-07-13 13:12:36 +0100

    Update js console errors A few console errors were being thrown if an element couldn't be found. Simple updates to order the call stack and prevent DOM warnings.

--- app/javascript/javascript/countrySelect.js
+++ app/javascript/javascript/countrySelect.js
@@ -1,4 +1,6 @@
 (async () => {
+  const selectCountry = document.querySelector("#booking_country");
+  if (!selectCountry) return;
   // current country
   const countryResult = await fetch("//freegeoip.app/json/");
   const countryJson = await countryResult.json();
@@ -6,8 +8,6 @@
   const phoneResult = await fetch(`https://restcountries.eu/rest/v2/alpha/${countryJson.country_code}`);
   const phoneJson = await phoneResult.json();
 
-  const selectCountry = document.querySelector("#booking_country");
-  if (!selectCountry) return;
   selectCountry.value = countryJson.country_code;
 
   const selectPhone = document.querySelector("#booking_phone_number");
