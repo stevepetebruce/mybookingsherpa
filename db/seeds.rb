3fd239945fff3951c8b96ff98014f6baecc49833
Alan Donohoe <alandonohoe@hotmail.com>
2019-07-29 15:57:52 +0100

    Fix seeds.rb.
    Since allergies and dietary conditions have become has_many associations to bookings,
    the seeds file was broken as it was still treating them as enumns on the booking model.
    
    Also, now we process the trip full price, multiplying it by 100 to turn it into a Stripe
    friendly cents version, the prices of the trips' full_cost was 100x too large.
    
    Also, moved the guide's password into an env var. - More proffessional.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-guide = Guide.create(email: "test_alpadventureguide@hotmail.com", name: "A Guide", password: "test_password!!!")
+guide = Guide.create(email: "test_alpadventureguide@hotmail.com", name: "A Guide", password: ENV.fetch("SEED_GUIDE_PASSWORD"))
 organisation = Organisation.create(currency: "eur", deposit_percentage: 10, full_payment_window_weeks: 6, name: "Alp Adventures Test", stripe_account_id: ENV.fetch("STRIPE_TEST_ACCOUNT_NUMBER"), subdomain: "alpadventurestest")
 OrganisationMembership.create(organisation: organisation, guide: guide, owner: true)
 
@@ -17,7 +17,7 @@ guide.trips.create(name: "Rewild",
                    end_date: Time.new(2019, 2, 27),
                    maximum_number_of_guests: 8,
                    description: "Get away from your busy life for 6 days to rewild in the overwhelming beauty of the backcountry. Escape the hustle and bustle of the ski area and discover the peace of the snowy mountains. Climb over cols, walk through woodland trails, build an igloo and enjoy incredible sunsets. At the campfire, tuck into a home-made meal and enjoy the magical experience of a night in an authentic mountain hut.",
-                   full_cost: 85000,
+                   full_cost: 850,
                    organisation: organisation,
                    slug: "Rewild".parameterize(separator: "_").truncate(80, omission: ""))
 guide.trips.create(name: "Ski Safari",
@@ -25,7 +25,7 @@ guide.trips.create(name: "Ski Safari",
                    end_date: Time.new(2019, 3, 13),
                    maximum_number_of_guests: 15,
                    description: "Explore the Grand Massif from three different angles. Every morning you'll step into your bindings from a different part of this amazing ski area. Your private guide will show you the best slopes, the deepest powder and the best lunches in the Grand Massif.",
-                   full_cost: 49900,
+                   full_cost: 499,
                    organisation: organisation,
                    slug: "Ski Safari".parameterize(separator: "_").truncate(80, omission: ""))
 guide.trips.create(name: "Backcountry Basecamp",
@@ -33,7 +33,7 @@ guide.trips.create(name: "Backcountry Basecamp",
                    end_date: Time.new(2019, 3, 24),
                    maximum_number_of_guests: 8,
                    description: "A 3 days touring adventure into the wild. A group of like-minded freeriders, tucked away in the background. A tent, an igloo, food and some drinks, ski's or boards and above all; a pair of skins to hike up the mountain and shred back down to basecamp!",
-                   full_cost: 59500,
+                   full_cost: 595,
                    organisation: organisation,
                    slug: "Backcountry Basecamp".parameterize(separator: "_").truncate(80, omission: ""))
 guide.trips.create(name: "Wintergration Station",
@@ -41,7 +41,7 @@ guide.trips.create(name: "Wintergration Station",
                    end_date: Time.new(2019, 4, 25),
                    maximum_number_of_guests: 15,
                    description: "A three days snow adventure; as soon on TV. Come over for a mountain adventure away from the crowded ski slopes. Enjoy the peace and beauty of the back country in the snowy mountains around Samoëns.",
-                   full_cost: 49900,
+                   full_cost: 499,
                    organisation: organisation,
                    slug: "Wintergration Station".parameterize(separator: "_").truncate(80, omission: ""))
 guide.trips.create(name: "A mindful snowshoe retreat",
@@ -49,7 +49,7 @@ guide.trips.create(name: "A mindful snowshoe retreat",
                    end_date: Time.new(2019, 3, 16),
                    maximum_number_of_guests: 8,
                    description: "6 days introduction into mindful snowshoeing. Join us on this trip for a daily snowshoe hike in a winter wonderland to introduce you to mindful snowshoeing. You will learn how to leave interfering thoughts behind, to enable your mind and body to feel energised, relaxed and fully present. And how about a 2 days snowshoe journey with a night in a hut, tucked away in the snowy mountains?!",
-                   full_cost: 125000,
+                   full_cost: 1250,
                    organisation: organisation,
                    slug: "A mindful snowshoe retreat".parameterize(separator: "_").truncate(80, omission: ""))
 guide.trips.create(name: "24h in the mountains",
@@ -57,7 +57,7 @@ guide.trips.create(name: "24h in the mountains",
                    end_date: Time.new(2019, 6, 10),
                    maximum_number_of_guests: 15,
                    description: "A quick escape from the ratrace. Getting away from your daily routine into the inknown of the mountains for just 24 hours can have a huge impact. Grab a flight to Geneva, jump in the van and join us for an awesome memorable mountain adventure.",
-                   full_cost: 15000,
+                   full_cost: 150,
                    organisation: organisation,
                    slug: "24h in the mountains".parameterize(separator: "_").truncate(80, omission: ""))
 guide.trips.create(name: "Best of the Haute Savoie",
@@ -65,7 +65,7 @@ guide.trips.create(name: "Best of the Haute Savoie",
                    end_date: Time.new(2019, 7, 16),
                    maximum_number_of_guests: 8,
                    description: "Six days of singletrack shredding in six different bike spots. Over 6 days you'll shred the best single tracks in Samoens, Les Portes du Soleil, la Clusaz, Salève, Chamonix or the hidden trails of la Vallée Verte. A combination of ski lifts and pedalling will get you to the best spots. You'll enjoy the view, open the suspension and drop the seats for another 1000 metre descent.",
-                   full_cost: 89500,
+                   full_cost: 895,
                    organisation: organisation,
                    slug: "Best of the Haute Savoie".parameterize(separator: "_").truncate(80, omission: ""))
 guide.trips.create(name: "Bucket Project",
@@ -73,7 +73,7 @@ guide.trips.create(name: "Bucket Project",
                    end_date: Time.new(2019, 8, 4),
                    maximum_number_of_guests: 8,
                    description: "A 4 days journey into the unknown. Four days of adventure: a journey off the beaten track, into the still. Things none of us have ever done before...",
-                   full_cost: 195000,
+                   full_cost: 1950,
                    organisation: organisation,
                    slug: "Bucket Project".parameterize(separator: "_").truncate(80, omission: ""))
 guide.trips.create(name: "Family adventures",
@@ -81,7 +81,7 @@ guide.trips.create(name: "Family adventures",
                    end_date: Time.new(2019, 8, 17),
                    maximum_number_of_guests: 16,
                    description: "A week full of outdoor action. Together with other parent(s) and their children, you will spend a full week in outdoor paradise \"la Vallée du Giffre. You will stay in a big and comfortable chalet with several double- and familyrooms, on a halfboard base. During 5 days we will organise you some awesome outdoor activities, like: mountainbiking, whitewater-rafting and rockclimbing. And what do you think about a two-days mountain adventure with a night in a remote mountainhut....?!",
-                   full_cost: 79900,
+                   full_cost: 799,
                    organisation: organisation,
                    slug: "Family adventures".parameterize(separator: "_").truncate(80, omission: ""))
 guide.trips.create(name: "Tour du Mont Blanc",
@@ -89,7 +89,7 @@ guide.trips.create(name: "Tour du Mont Blanc",
                    end_date: Time.new(2019, 9, 17),
                    maximum_number_of_guests: 7,
                    description: "6 days enduro bike trip around the highest mountain of western Europe. An awesome enduro bike trip around the highest mountain of western Europe, that is the famous Tour du Mont Blanc. From Chamonix we ride clockwise through Martigny, to Italy and back into France. We ride through authentic villages, across rustic alpine meadows, surrounded by impressive glaciers and the highest peaks of the Alps. We spend the nights in basic but cosy 2star mountain hotels on a halfboard base. This is an unforgettable bike experience with trails and views you will never forget!",
-                   full_cost: 149500,
+                   full_cost: 1495,
                    organisation: organisation,
                    slug: "Tour du Mont Blanc".parameterize(separator: "_").truncate(80, omission: ""))
 guide.trips.create(name: "Trans74",
@@ -97,7 +97,7 @@ guide.trips.create(name: "Trans74",
                    end_date: Time.new(2019, 7, 7),
                    maximum_number_of_guests: 8,
                    description: "a 6 days bike adventures through the Haute Savoie. Starting from Samoëns we link up the best bike spots of the Haute Savoie. We use a skilift to climb the first hundreds of meters, followed by a climb to arrive at a remote mountainhut where we'll spend the night. An early sunset wakes you up, while you get ready for another awesome day of singletrack shredding.",
-                   full_cost: 89500,
+                   full_cost: 895,
                    organisation: organisation,
                    slug: "Trans74".parameterize(separator: "_").truncate(80, omission: ""))
 
@@ -113,21 +113,19 @@ end
 
 # Full details:
 15.times.each do
-  show_allergies = [true, false].sample
-  show_dietary_requirements = [true, false].sample
-  show_other_information = [true, false].sample
+  create_allergies = [true, false].sample
+  create_dietary_requirements = [true, false].sample
+  create_other_information = [true, false].sample
 
-  allergies = show_allergies ? %i[dairy eggs nuts soya].sample : nil
-  dietary_requirements = show_dietary_requirements ? %i[other vegan vegetarian].sample : nil
-  other_information = show_other_information ? Faker::Lorem.sentence : nil
+  allergy_name = %i[dairy eggs nuts soya].sample
+  dietary_requirement_name = %i[other vegan vegetarian].sample
+  other_information = create_other_information ? Faker::Lorem.sentence : nil
 
   email = Faker::Internet.email
   name = Faker::Name.name
 
-  guest = Guest.create(allergies_booking: allergies,
-                       country_booking: Faker::Address.country_code,
+  guest = Guest.create(country_booking: Faker::Address.country_code,
                        date_of_birth_booking: Faker::Date.birthday(18, 65),
-                       dietary_requirements: dietary_requirements,
                        email: email,
                        email_booking: email,
                        other_information_booking: other_information,
@@ -138,6 +136,9 @@ end
                        phone_number_booking: Faker::PhoneNumber.cell_phone)
 
   puts "Guest errors: #{guest.errors.full_messages}" if guest.errors.full_messages.present?
+
+  guest.allergies.create(name: allergy_name) if create_allergies
+  guest.dietary_requirements.create(name: dietary_requirement_name) if create_dietary_requirements
 end
 
 # Bookings
@@ -157,4 +158,4 @@ end
 
 # Developer friendly logging
 Trip.all.each { |trip| puts "Trip id: #{trip.id}" }
-puts "Guide details: email: test_alpadventureguide@hotmail.com. Password: test_password!!!"
+puts "Guide details: email: test_alpadventureguide@hotmail.com. Password: see SEED_GUIDE_PASSWORD env var"
