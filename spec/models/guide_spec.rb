74aa77c310f62197294d41f11b5426d928736b52
Alan Donohoe <alandonohoe@hotmail.com>
2019-08-04 10:16:23 +0100

    Coding standards

--- spec/models/guide_spec.rb
+++ spec/models/guide_spec.rb
@@ -1,13 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Guide, type: :model do
-  describe 'associations' do
+  describe "associations" do
     it { is_expected.to have_and_belong_to_many(:trips) }
     it { should have_many(:organisations).through(:organisation_memberships) }
   end
 
-  describe 'validations' do
-    context 'email' do
+  describe "validations" do
+    context "email" do
       it { should allow_value(Faker::Internet.email).for(:email) }
       it { should allow_value("alan.donohoe@mybookingsherpa.com").for(:email) }
       it { should allow_value("ad591@sussex.ac.uk").for(:email) }
@@ -16,11 +16,11 @@ RSpec.describe Guide, type: :model do
       it { should validate_presence_of(:email) }
       it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
     end
-    context 'name' do
-      it { should_not allow_value('<SQL INJECTION>').for(:name) }
+    context "name" do
+      it { should_not allow_value("<SQL INJECTION>").for(:name) }
       it { should allow_value(Faker::Lorem.word).for(:name) }
     end
-    context 'phone_number' do
+    context "phone_number" do
       it { should allow_value(Faker::PhoneNumber.cell_phone).for(:phone_number) }
       it { should_not allow_value(Faker::Lorem.word).for(:phone_number) }
     end
