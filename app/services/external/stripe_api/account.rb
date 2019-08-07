71ff610ec80c873d81320bc621071d42a0f7e01b
Alan Donohoe <alandonohoe@hotmail.com>
2019-08-07 19:35:05 +0100

    Allow the creation of Individual Stripe Accounts.
    Part of the work to allow new Guides to sign up for new Stripe Account
    and bank account to accept payments themsevles.
    
    Work in progress: but this sends the sensitive data to Stripe, which
    returns a token for the account which we then use in a background job
    to create the account proper.
    
    Further work required - loads of test coverage, design, UX, security,
    creating the bank account asccoiated with the Stripe account and uploading
    the ID documents.

---
+++ app/services/external/stripe_api/account.rb
@@ -0,0 +1,27 @@
+module External
+  # Encapsulates all Stripe API Connected Accounts functionality
+  # Ref: https://stripe.com/docs/api/accounts
+  module StripeApi
+    class Account < External::StripeApi::Base
+      def initialize(account_token)
+        @account_token = account_token
+        @use_test_api = true
+        initialize_key
+      end
+
+      # https://stripe.com/docs/connect/account-tokens#create-account
+      def create
+        Stripe::Account.create(
+          {
+            account_token: @account_token,
+            type: "custom"
+          }
+        )
+      end
+
+      def self.create(account_token)
+        new(account_token).create
+      end
+    end
+  end
+end
