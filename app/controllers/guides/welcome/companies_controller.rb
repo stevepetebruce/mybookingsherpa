9c5ad41139247c168031b4331a6bba9bfa5b6074
Alan Donohoe <alandonohoe@hotmail.com>
2019-08-11 06:52:07 +0100

    Create Stripe company accounts - with Stripe Account, bank account and multiple owners and directors.

--- app/controllers/guides/welcome/companies_controller.rb
+++ app/controllers/guides/welcome/companies_controller.rb
@@ -2,8 +2,46 @@ module Guides
   module Welcome
     class CompaniesController < ApplicationController
       before_action :authenticate_guide!
+      before_action :set_current_organisation
 
       def new; end
+
+      def create
+        if creating_stripe_account?
+          @current_organisation.update(stripe_account_id: raw_stripe_api_response.id)
+          redirect_to new_guides_welcome_company_path # need to expose any errors
+        else
+          create_bank_account
+          redirect_to new_guides_welcome_director_path
+        end
+      end
+
+      private
+
+      def create_bank_account
+        # TODO: create / capture raw_stripe_api_response
+        # Add a field to organisation - to record that bank account has been created...
+        Stripe::Account.create_external_account(
+          @current_organisation.stripe_account_id,
+          {
+            external_account: params[:token_account],
+          }
+        )
+      end
+
+      def creating_stripe_account?
+        @current_organisation.stripe_account_id.blank?
+      end
+
+      def raw_stripe_api_response
+        # TODO: create / capture raw_stripe_api_response
+        @raw_stripe_api_response ||= External::StripeApi::Account.create(params[:token_account])
+      end
+
+      def set_current_organisation
+        # TODO: when a Guide owns more than one organisation, will need a way to choose btwn them.
+        @current_organisation ||= current_guide.organisation_memberships.owners.first.organisation
+      end
     end
   end
 end
---
+++ app/javascript/controllers/stripe_account_company_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "form", "addressLine1", "addressLine2", "addressCity", "addressState", "addressPostalCode", "addressCountryCode", "name", "phone", "tax_id", "tokenAccount", "vat_id" ]
+
+  connect() {
+    const controller = this;
+    const form = this.formTarget;
+    const stripe = Stripe(this.data.get("key"));
+    // handleFormSubmission(); // eventually - move all the below into a neater function...
+    form.addEventListener('submit', async function (event) {
+      event.preventDefault();
+      // ref: https://stripe.com/docs/api/tokens/create_account
+      const accountResult = await stripe.createToken("account", {
+        business_type: "company",
+        company: {
+          name: controller.nameTarget.value,
+          address: {
+            line1: controller.addressLine1Target.value,
+            line2: controller.addressLine2Target.value,
+            city: controller.addressCityTarget.value,
+            state: controller.addressStateTarget.value,
+            postal_code: controller.addressPostalCodeTarget.value,
+            country: controller.addressCountryCodeTarget.value
+          },
+        },
+        tos_shown_and_accepted: true,
+      });
+
+      if (accountResult.token) {
+        controller.tokenAccountTarget.setAttribute("value", accountResult.token.id);
+        form.submit();
+      }
+    });
+  }
+}
