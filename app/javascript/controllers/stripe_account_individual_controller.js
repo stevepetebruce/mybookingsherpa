512f3b9d07c73a24f2dd3f461c63b181c47f26f5
Alan Donohoe <alandonohoe@hotmail.com>
2019-08-13 18:42:24 +0100

    Add validation to solo personal details page

--- app/javascript/controllers/stripe_account_individual_controller.js
+++ app/javascript/controllers/stripe_account_individual_controller.js
@@ -1,11 +1,20 @@
+// Ref: https://stripe.com/docs/api/tokens/create_account
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["addressLine1", "addressLine2", "addressCity",
-                    "addressState", "addressPostalCode", "addressCountry",
-                    "businessDetails", "dateOfBirth", "email", "firstName",
-                    "form", "gender", "lastName", "personalDetails", "phone",
+  static targets = ["addressLine1", "addressLine1Error", "addressLine2",
+                    "addressLine2Error", "addressCity", "addressCityError",
+                    "addressState", "addressStateError", "addressPostalCode",
+                    "addressPostalCodeError", "addressCountry",
+                    "addressCountryError", "businessDetails", "dateOfBirth",
+                    "dateOfBirthError", "email", "emailError", "firstName",
+                    "firstNameError", "form", "gender", "genderError", "lastName",
+                    "lastNameError", "personalDetails", "phone", "phoneError",
                     "submitBtn", "tokenAccount"]
+
+  // TODO: loop thru: requiredPersonalDetails where we need to....
+  static requiredPersonalDetails = [this.emailTarget, this.firstNameTarget,
+                                    this.lastNameTarget, this.dateOfBirthTarget]
 
   connect() {
     this.addFormSubmissionHandler();
@@ -51,6 +60,20 @@ export default class extends Controller {
     });
   }
 
+  allPersonalFieldsComplete() {
+    // TODO: loop thru: requiredPersonalDetails and check that...
+    if(this.isEmpty(this.emailTarget) || this.isEmpty(this.firstNameTarget) ||
+        this.isEmpty(this.lastNameTarget) || this.isEmpty(this.dateOfBirthTarget)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  isEmpty(el) {
+    return el.value.length === 0;
+  }
+
   enableSubmitBtn(el) {
     if (el.target.checked) { 
       this.submitBtnTarget.disabled = false;
@@ -59,8 +82,47 @@ export default class extends Controller {
     }
   }
 
-  toggleDetails() {
-    this.personalDetailsTargets.forEach(function(target) { target.classList.add("d-none"); });
-    this.businessDetailsTargets.forEach(function(target) { target.classList.remove("d-none"); });
+  proceedIfValid() {
+    if (this.allPersonalFieldsComplete()) {
+      this.personalDetailsTargets.forEach(function(target) { target.classList.add("d-none"); });
+      this.businessDetailsTargets.forEach(function(target) { target.classList.remove("d-none"); });
+    } else {
+      this.showValidationErrors();
+    }
+  }
+
+  showValidationErrors() {
+    // TODO: loop thru: requiredPersonalDetails and check that...
+    if(this.isEmpty(this.emailTarget)) {
+      this.emailTarget.classList.add("is-invalid");
+      this.emailErrorTarget.classList.remove("d-none");
+    } else {
+      this.emailTarget.classList.add("is-valid");
+      this.emailErrorTarget.classList.add("d-none");
+    }
+
+    if(this.isEmpty(this.firstNameTarget)) {
+      this.firstNameTarget.classList.add("is-invalid");
+      this.firstNameErrorTarget.classList.remove("d-none");
+    } else {
+      this.firstNameTarget.classList.add("is-valid");
+      this.firstNameErrorTarget.classList.add("d-none");
+    }
+
+    if(this.isEmpty(this.lastNameTarget)) {
+      this.lastNameTarget.classList.add("is-invalid");
+      this.lastNameErrorTarget.classList.remove("d-none");
+    } else {
+      this.lastNameTarget.classList.add("is-valid");
+      this.lastNameErrorTarget.classList.add("d-none");
+    }
+
+    if(this.isEmpty(this.dateOfBirthTarget)) {
+      this.dateOfBirthTarget.classList.add("is-invalid");
+      this.dateOfBirthErrorTarget.classList.remove("d-none");
+    } else {
+      this.dateOfBirthTarget.classList.add("is-valid");
+      this.dateOfBirthErrorTarget.classList.add("d-none");
+    }
   }
 }
