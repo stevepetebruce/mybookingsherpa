2ba34c5daae3a7b527b3a67c699f245f97044f82
Alan Donohoe <alandonohoe@hotmail.com>
2019-08-17 12:10:56 +0100

    Add validation to solo business details page and refactor Stimulus stripe_account_individual_controller.

--- app/javascript/controllers/stripe_account_individual_controller.js
+++ app/javascript/controllers/stripe_account_individual_controller.js
@@ -2,80 +2,44 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["addressLine1", "addressLine1Error", "addressLine2",
-                    "addressLine2Error", "addressCity", "addressCityError",
-                    "addressState", "addressStateError", "addressPostalCode",
-                    "addressPostalCodeError", "addressCountry",
-                    "addressCountryError", "businessDetails", "dateOfBirth",
-                    "dateOfBirthError", "email", "emailError", "firstName",
-                    "firstNameError", "form", "gender", "genderError", "lastName",
-                    "lastNameError", "personalDetails", "phone", "phoneError",
-                    "submitBtn", "tokenAccount"]
-
-  // TODO: loop thru: requiredPersonalDetails where we need to....
-  static requiredPersonalDetails = [this.emailTarget, this.firstNameTarget,
-                                    this.lastNameTarget, this.dateOfBirthTarget]
+  static targets = ["addressLine1", "addressLine2", "addressCity",
+                    "addressState", "addressPostalCode", "addressCountry",
+                    "businessDetailsWrapper", "dateOfBirth", "email",
+                    "firstName", "form", "gender", "lastName",
+                    "personalDetailsWrapper", "phone", "requiredBusiness",
+                    "requiredPersonal", "stripeTosCheckBox", "submitBtn",
+                    "tokenAccount"]
 
   connect() {
     this.addFormSubmissionHandler();
+    this.enableSubmitBtn();
   }
 
   addFormSubmissionHandler() {
     const controller = this;
-    const form = this.formTarget;
-    const stripe = Stripe(this.data.get("key"));
-
-    form.addEventListener("submit", async function (event) {
+    this.formTarget.addEventListener("submit", async function (event) {
       event.preventDefault();
 
-      const accountResult = await stripe.createToken("account", {
-        business_type: "individual",
-        individual: {
-          address: {
-            line1: controller.addressLine1Target.value,
-            line2: controller.addressLine2Target.value,
-            city: controller.addressCityTarget.value,
-            state: controller.addressStateTarget.value,
-            postal_code: controller.addressPostalCodeTarget.value,
-            country: controller.addressCountryTarget.value
-          },
-          dob: {
-            day: controller.dateOfBirthTarget.value.split("-")[2],
-            month: controller.dateOfBirthTarget.value.split("-")[1],
-            year: controller.dateOfBirthTarget.value.split("-")[0]
-          },
-          email: controller.emailTarget.value,
-          first_name: controller.firstNameTarget.value,
-          gender: controller.genderTarget.value,
-          last_name: controller.lastNameTarget.value
-          //phone: document.querySelector("#phone").value // "Not a valid phone number"
-        },
-        tos_shown_and_accepted: true,
-      });
-
-      if (accountResult.token) {
-        controller.tokenAccountTarget.setAttribute("value", accountResult.token.id);
-        form.submit();
+      if (controller.allBusinessFieldsComplete()) {
+        controller.requestStripeTokenAndSubmitForm();
+      } else {
+        controller.showValidationErrors();
       }
     });
   }
 
+  allBusinessFieldsComplete() {
+    return this.requiredBusinessTargets.
+      every((requiredField) => requiredField.value.length != 0);
+  }
+
   allPersonalFieldsComplete() {
-    // TODO: loop thru: requiredPersonalDetails and check that...
-    if(this.isEmpty(this.emailTarget) || this.isEmpty(this.firstNameTarget) ||
-        this.isEmpty(this.lastNameTarget) || this.isEmpty(this.dateOfBirthTarget)) {
-      return false;
-    } else {
-      return true;
-    }
+    return this.requiredPersonalTargets.
+      every((requiredField) => requiredField.value.length != 0);
   }
 
-  isEmpty(el) {
-    return el.value.length === 0;
-  }
-
-  enableSubmitBtn(el) {
-    if (el.target.checked) { 
+  enableSubmitBtn() {
+    if (this.stripeTosCheckBoxTarget.checked) {
       this.submitBtnTarget.disabled = false;
     } else {
       this.submitBtnTarget.disabled = true;
@@ -84,45 +48,59 @@ export default class extends Controller {
 
   proceedIfValid() {
     if (this.allPersonalFieldsComplete()) {
-      this.personalDetailsTargets.forEach(function(target) { target.classList.add("d-none"); });
-      this.businessDetailsTargets.forEach(function(target) { target.classList.remove("d-none"); });
+      this.personalDetailsWrapperTarget.classList.add("d-none");
+      this.businessDetailsWrapperTarget.classList.remove("d-none");
     } else {
       this.showValidationErrors();
     }
   }
 
-  showValidationErrors() {
-    // TODO: loop thru: requiredPersonalDetails and check that...
-    if(this.isEmpty(this.emailTarget)) {
-      this.emailTarget.classList.add("is-invalid");
-      this.emailErrorTarget.classList.remove("d-none");
-    } else {
-      this.emailTarget.classList.add("is-valid");
-      this.emailErrorTarget.classList.add("d-none");
-    }
+  async requestStripeTokenAndSubmitForm() {
+    const controller = this;
+    const form = this.formTarget;
+    const stripe = Stripe(this.data.get("key"));
 
-    if(this.isEmpty(this.firstNameTarget)) {
-      this.firstNameTarget.classList.add("is-invalid");
-      this.firstNameErrorTarget.classList.remove("d-none");
-    } else {
-      this.firstNameTarget.classList.add("is-valid");
-      this.firstNameErrorTarget.classList.add("d-none");
-    }
+    const accountResult = await stripe.createToken("account", {
+      business_type: "individual",
+      individual: {
+        address: {
+          line1: controller.addressLine1Target.value,
+          line2: controller.addressLine2Target.value,
+          city: controller.addressCityTarget.value,
+          state: controller.addressStateTarget.value,
+          postal_code: controller.addressPostalCodeTarget.value,
+          country: controller.addressCountryTarget.value
+        },
+        dob: {
+          day: controller.dateOfBirthTarget.value.split("-")[2],
+          month: controller.dateOfBirthTarget.value.split("-")[1],
+          year: controller.dateOfBirthTarget.value.split("-")[0]
+        },
+        email: controller.emailTarget.value,
+        first_name: controller.firstNameTarget.value,
+        gender: controller.genderTarget.value,
+        last_name: controller.lastNameTarget.value
+        //phone: document.querySelector("#phone").value // "Not a valid phone number"
+      },
+      tos_shown_and_accepted: true,
+    });
 
-    if(this.isEmpty(this.lastNameTarget)) {
-      this.lastNameTarget.classList.add("is-invalid");
-      this.lastNameErrorTarget.classList.remove("d-none");
-    } else {
-      this.lastNameTarget.classList.add("is-valid");
-      this.lastNameErrorTarget.classList.add("d-none");
-    }
-
-    if(this.isEmpty(this.dateOfBirthTarget)) {
-      this.dateOfBirthTarget.classList.add("is-invalid");
-      this.dateOfBirthErrorTarget.classList.remove("d-none");
-    } else {
-      this.dateOfBirthTarget.classList.add("is-valid");
-      this.dateOfBirthErrorTarget.classList.add("d-none");
+    if (accountResult.token) {
+      controller.tokenAccountTarget.setAttribute("value", accountResult.token.id);
+      form.submit();
     }
   }
+
+  showValidationErrors() {
+    this.requiredPersonalTargets.forEach(this.toggleValidationError);
+    this.requiredBusinessTargets.forEach(this.toggleValidationError);
+  }
+
+  toggleValidationError(target) {
+    if (target.value.length == 0) {
+      target.nextElementSibling.classList.remove("d-none");
+    } else {
+      target.nextElementSibling.classList.add("d-none");
+    }
+  }  
 }
