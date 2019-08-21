f39f9834e9d215962f9e4c747e4eaef09f49c44a
Alan Donohoe <alandonohoe@hotmail.com>
2019-08-21 04:48:14 +0100

    Incorporate latest solo stripe account work into stripe company account work

--- app/controllers/guides/welcome/companies_controller.rb
+++ app/controllers/guides/welcome/companies_controller.rb
@@ -1,41 +1,28 @@
 module Guides
   module Welcome
     class CompaniesController < ApplicationController
+      layout "onboarding"
       before_action :authenticate_guide!
       before_action :set_current_organisation
 
       def new; end
 
       def create
-        if creating_stripe_account?
-          @current_organisation.update(stripe_account_id: raw_stripe_api_response.id)
-          redirect_to new_guides_welcome_company_path # need to expose any errors
-        else
-          create_bank_account
+        if @current_organisation.update(stripe_account_id: stripe_account.id)
+          # redirect_to new_guides_welcome_bank_account_path
           redirect_to new_guides_welcome_director_path
+        else
+          redirect_to new_guides_welcome_solo_path # need to expose any errors
         end
       end
 
       private
 
-      def create_bank_account
+      def stripe_account
         # TODO: create / capture raw_stripe_api_response
-        # Add a field to organisation - to record that bank account has been created...
-        Stripe::Account.create_external_account(
-          @current_organisation.stripe_account_id,
-          {
-            external_account: params[:token_account],
-          }
-        )
-      end
-
-      def creating_stripe_account?
-        @current_organisation.stripe_account_id.blank?
-      end
-
-      def raw_stripe_api_response
-        # TODO: create / capture raw_stripe_api_response
-        @raw_stripe_api_response ||= External::StripeApi::Account.create(params[:token_account])
+        # TODO: move this to a background job? - and reveal to Guide any errors in my account view...?
+        # Would then need another way (other than checking @current_organisation has stripe_account_id - in conditional "creating_stripe_account?" )
+        @stripe_account ||= External::StripeApi::Account.create(params[:token_account])
       end
 
       def set_current_organisation
