77ea47cd29cd53cc920cdf7e0581b54dd8d33487
Alan Donohoe <alandonohoe@hotmail.com>
2019-08-21 04:28:29 +0100

    fix enable submit btn

--- app/javascript/controllers/stripe_account_individual_controller.js
+++ app/javascript/controllers/stripe_account_individual_controller.js
@@ -1,52 +1,102 @@
-import { Controller } from "stimulus"
+// Ref: https://stripe.com/docs/api/tokens/create_account
+import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = [ "form", "addressLine1", "addressLine2", "addressCity", "addressState", "addressPostalCode", "addressCountryCode", "phone", "dobDay", "dobMonth", "dobYear", "email", "firstName", "lastName", "maidenName", "gender", "tokenAccount" ]
+  static targets = ["addressLine1", "addressLine2", "addressCity",
+                    "addressState", "addressPostalCode", "addressCountry",
+                    "businessDetailsWrapper", "dateOfBirth", "email",
+                    "firstName", "form", "gender", "lastName",
+                    "personalDetailsWrapper", "phone", "requiredBusiness",
+                    "requiredPersonal", "stripeTosCheckBox", "submitBtn",
+                    "tokenAccount"]
 
   connect() {
+    this.addFormSubmissionHandler();
+    this.enableSubmitBtn();
+  }
+
+  addFormSubmissionHandler() {
     const controller = this;
-    const form = this.formTarget;
-    const stripe = Stripe(this.data.get("key"));
-    // handleFormSubmission(); // eventually - move all the below into a neater function...
-    form.addEventListener('submit', async function (event) {
+    this.formTarget.addEventListener("submit", async function (event) {
       event.preventDefault();
 
-      const accountResult = await stripe.createToken("account", {
-        business_type: "individual",
-        individual: {
-          address: {
-            line1: controller.addressLine1Target.value, // document.querySelector("#address_line1").value,
-            line2: controller.addressLine2Target.value, // document.querySelector("#address_line2").value,
-            city: controller.addressCityTarget.value, // document.querySelector("#address_city").value,
-            state: controller.addressStateTarget.value, // document.querySelector("#address_state").value,
-            postal_code: controller.addressPostalCodeTarget.value, // document.querySelector("#address_postal_code").value,
-            country: controller.addressCountryCodeTarget.value // document.querySelector("#address_country_code").value
-          },
-          dob: {
-            day: controller.dobDayTarget.value, // document.querySelector("#dob_day").value,
-            month: controller.dobMonthTarget.value, // document.querySelector("#dob_month").value,
-            year: controller.dobYearTarget.value // document.querySelector("#dob_year").value,
-          },
-          email: controller.emailTarget.value, // document.querySelector("#email").value,
-          first_name: controller.firstNameTarget.value, // document.querySelector("#first_name").value,
-          last_name: controller.lastNameTarget.value, // document.querySelector("#last_name").value,
-          gender: controller.genderTarget.value, // document.querySelector("#gender").value,
-          maiden_name: controller.maidenNameTarget.value// document.querySelector("#maiden_name").value,
-          //phone: document.querySelector("#phone").value // "Not a valid phone number"
-        },
-        // exernal_account: { // TODO: need to use this: https://stripe.com/docs/api/tokens/create_bank_account
-        //   account_number: document.querySelector("#stripe_account_bank_account_number").value,
-        //   country: document.querySelector("#stripe_account_individual_address_country_code").value,
-        //   currency: document.querySelector("#stripe_account_default_currency").value,
-        //   object: "bank_account"
-        // },
-        tos_shown_and_accepted: true,
-      });
-
-      if (accountResult.token) {
-        controller.tokenAccountTarget.setAttribute("value", accountResult.token.id);
-        form.submit();
+      if (controller.allBusinessFieldsComplete()) {
+        controller.requestStripeTokenAndSubmitForm();
+      } else {
+        controller.showValidationErrors();
       }
     });
   }
+
+  allBusinessFieldsComplete() {
+    return this.requiredBusinessTargets.
+      every((requiredField) => requiredField.value.length !== 0);
+  }
+
+  allPersonalFieldsComplete() {
+    return this.requiredPersonalTargets.
+      every((requiredField) => requiredField.value.length !== 0);
+  }
+
+  enableSubmitBtn() {
+    this.submitBtnTarget.disabled = !this.stripeTosCheckBoxTarget.checked;
+  }
+
+  proceedIfValid() {
+    if (this.allPersonalFieldsComplete()) {
+      this.personalDetailsWrapperTarget.classList.add("d-none");
+      this.businessDetailsWrapperTarget.classList.remove("d-none");
+    } else {
+      this.showValidationErrors();
+    }
+  }
+
+  async requestStripeTokenAndSubmitForm() {
+    const controller = this;
+    const form = this.formTarget;
+    const stripe = Stripe(this.data.get("key"));
+
+    const accountResult = await stripe.createToken("account", {
+      business_type: "individual",
+      individual: {
+        address: {
+          line1: controller.addressLine1Target.value,
+          line2: controller.addressLine2Target.value,
+          city: controller.addressCityTarget.value,
+          state: controller.addressStateTarget.value,
+          postal_code: controller.addressPostalCodeTarget.value,
+          country: controller.addressCountryTarget.value
+        },
+        dob: {
+          day: controller.dateOfBirthTarget.value.split("-")[2],
+          month: controller.dateOfBirthTarget.value.split("-")[1],
+          year: controller.dateOfBirthTarget.value.split("-")[0]
+        },
+        email: controller.emailTarget.value,
+        first_name: controller.firstNameTarget.value,
+        gender: controller.genderTarget.value,
+        last_name: controller.lastNameTarget.value
+        //phone: document.querySelector("#phone").value // "Not a valid phone number"
+      },
+      tos_shown_and_accepted: true,
+    });
+
+    if (accountResult.token) {
+      controller.tokenAccountTarget.setAttribute("value", accountResult.token.id);
+      form.submit();
+    }
+  }
+
+  showValidationErrors() {
+    this.requiredPersonalTargets.forEach(this.toggleValidationError);
+    this.requiredBusinessTargets.forEach(this.toggleValidationError);
+  }
+
+  toggleValidationError(target) {
+    if (target.value.length === 0) {
+      target.nextElementSibling.classList.remove("d-none");
+    } else {
+      target.nextElementSibling.classList.add("d-none");
+    }
+  }  
 }
