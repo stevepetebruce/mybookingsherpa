792da7c584f430d2e281b4e4c42a73ef78c8528b
Alan Donohoe <alandonohoe@hotmail.com>
2019-08-26 14:35:35 +0100

    Show server side Stripe API errors.
    Part of the work for on-boarding new guides.
    
    We need to show user feedback when there's an issue when creating a
    new Stripe account.

--- app/controllers/guides/welcome/solos_controller.rb
+++ app/controllers/guides/welcome/solos_controller.rb
@@ -14,25 +14,39 @@ module Guides
       def new; end
 
       def create
-        if @current_organisation.update(stripe_account_id: stripe_account.id)
+        if create_stripe_account
           redirect_to new_guides_welcome_bank_account_path
         else
-          redirect_to new_guides_welcome_solo_path # need to expose any errors
+          flash.now[:alert] = "Problem creating account. #{error_message}"
+          render :new
         end
       end
 
       private
 
-      def stripe_account
+      def create_stripe_account
         # TODO: create / capture raw_stripe_api_response
         # TODO: move this to a background job? - and reveal to Guide any errors in my account view...?
         # Would then need another way (other than checking @current_organisation has stripe_account_id - in conditional "creating_stripe_account?" )
-        @stripe_account ||= External::StripeApi::Account.create(params[:token_account])
+        @current_organisation.update(stripe_account_id: stripe_account.id)
+      rescue Stripe::InvalidRequestError => e
+        @stripe_error_message = e.message
+        false
+      end
+
+      def error_message
+        return @stripe_error_message if defined?(@stripe_error_message)
+
+        @current_organisation.errors.full_messages.to_sentence
       end
 
       def set_current_organisation
         # TODO: when a Guide owns more than one organisation, will need a way to choose btwn them.
         @current_organisation ||= current_guide.organisation_memberships.owners.first.organisation
+      end
+
+      def stripe_account
+        @stripe_account ||= External::StripeApi::Account.create(params[:token_account])
       end
     end
   end
