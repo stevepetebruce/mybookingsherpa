00ac04695988a49ea5c1e2a323442f20a2a500c0
Alan Donohoe <alandonohoe@hotmail.com>
2019-09-13 09:43:27 +0100

    Fix missing in trial banner in trip spreadsheet view.
    Part of the work to onboard new guides.
    
    The in trial banner wasn't being shown in this view as we were not
    setting the current_organisation which is used to check if the guide
    is in trial...

--- app/controllers/guides/trips/bookings_controller.rb
+++ app/controllers/guides/trips/bookings_controller.rb
@@ -2,9 +2,16 @@ module Guides
   module Trips
     class BookingsController < ApplicationController
       before_action :authenticate_guide!
+      before_action :set_current_organisation, only: %i[index]
       before_action :set_trip
 
       private
+
+      def set_current_organisation
+        # TODO: when a Guide owns more than one organisation, will need a way to choose btwn them.
+        # TODO: move this to a base controller / concern?
+        @current_organisation ||= current_guide&.organisation_memberships&.owners&.first&.organisation
+      end
 
       def set_trip
         @trip = current_guide.trips.find(params[:trip_id]) 
