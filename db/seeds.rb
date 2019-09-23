9d50725a7a96c5d69a4b92c65e333740c77e3ea1
Alan Donohoe <alandonohoe@hotmail.com>
2019-09-23 15:05:04 +0100

    Only run db:seeds when environment is development.
    This was screwing up my test DB and we don't want this running in production now.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+return unless Rails.env.development?
+
 guide = Guide.create(email: "test_alpadventureguide@hotmail.com", name: "A Guide", password: ENV.fetch("SEED_GUIDE_PASSWORD"))
 organisation = Organisation.create(currency: "eur", deposit_percentage: 10, full_payment_window_weeks: 6, name: "Alp Adventures Test", stripe_account_id: ENV.fetch("STRIPE_TEST_ACCOUNT_NUMBER"), subdomain: "alpadventurestest")
 OrganisationMembership.create(organisation: organisation, guide: guide, owner: true)
