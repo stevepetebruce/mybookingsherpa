1360fe9e00c885583d95687d11ae6af475dad2c3
Alan Donohoe <alandonohoe@hotmail.com>
2019-10-08 05:48:47 +0100

    Add StripeApi::AccountLink service model.
    This creates the AccountLink needed to direct new guides to Stripe's
    hosted onboarding process.
    
    May need unit test coverage but is covered by request spec at the moment.
    
    ref: https://stripe.com/docs/api/account_links

---
+++ app/services/external/stripe_api/account_link.rb
@@ -0,0 +1,30 @@
+module External
+  module StripeApi
+    # Create a URL to Stripe's hosted onboarding process
+    # Ref: https://stripe.com/docs/api/account_links
+    class AccountLink < External::StripeApi::Base
+      def initialize(stripe_account_id, failure_url, success_url, type)
+        @stripe_account_id = stripe_account_id
+        @failure_url = failure_url
+        @success_url = success_url
+        @type = type
+        @use_test_api = false # We will never be creating these in the test API.
+        initialize_key # TODO: use super and initialize in base controller
+      end
+
+      def create
+        Stripe::AccountLink.create({
+          account: @stripe_account_id,
+          collect: "currently_due",
+          failure_url: @failure_url,
+          success_url: @success_url,
+          type: @type
+        }).url
+      end
+
+      def self.create(stripe_account_id, failure_url:, success_url:, type: "custom_account_verification")
+        new(stripe_account_id, failure_url, success_url, type).create
+      end
+    end
+  end
+end
