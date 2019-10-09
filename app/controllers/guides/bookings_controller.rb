3e574c87036a68c4f2527ce2b7851cff81dac5d0
Alan Donohoe <alandonohoe@hotmail.com>
2019-10-09 20:57:40 +0100

    Refactor controllers to handle new onboarding process.

--- app/controllers/guides/bookings_controller.rb
+++ app/controllers/guides/bookings_controller.rb
@@ -1,9 +1,17 @@
 module Guides
   class BookingsController < ApplicationController
     before_action :authenticate_guide!
+    before_action :set_current_organisation
 
     def show
       @booking = current_guide.bookings.find(params[:id])
+    end
+
+    private
+
+    def set_current_organisation
+      # TODO: when a Guide owns more than one organisation, will need a way to choose btwn them.
+      @current_organisation ||= current_guide&.organisation_memberships&.owners&.first&.organisation
     end
   end
 end
