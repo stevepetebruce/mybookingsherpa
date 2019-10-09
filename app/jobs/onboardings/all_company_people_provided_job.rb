c24268e8a0244bbdd1944e183ea67f880b561bd2
Alan Donohoe <alandonohoe@hotmail.com>
2019-10-09 20:30:24 +0100

    Delete all, now redundant, files that we were using for onboarding.
    These have been replaced by the new Stripe hosted onboarding process.

--- app/jobs/onboardings/all_company_people_provided_job.rb
+++
@@ -1,9 +0,0 @@
-module Onboardings
-  class AllCompanyPeopleProvidedJob < ApplicationJob
-    queue_as :default
-
-    def perform(organisation, account_token)
-      External::StripeApi::Account.new(account_token: account_token).update(organisation.stripe_account_id)
-    end
-  end
-end
