c61d3cdc4be6add2d5d6da2cc790915dc9851445
Alan Donohoe <alandonohoe@hotmail.com>
2019-12-08 13:49:56 +0000

    Only run Trips::OutstandingPaymentDueJob if organisation is out of trial.
    This job throws an error if the organisation is in trial...

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -26,7 +26,7 @@ class Trip < ApplicationRecord
   has_and_belongs_to_many :guides
   has_many :organisation_memberships, through: :guides
 
-  delegate :logo_image, :name, :stripe_account_id, :subdomain_or_www, to: :organisation, prefix: true
+  delegate :logo_image, :name, :on_trial?, :stripe_account_id, :subdomain_or_www, to: :organisation, prefix: true
 
   scope :future_trips, -> { end_date_asc.where("end_date > ?", Time.zone.yesterday.end_of_day) }
   scope :past_trips, -> { end_date_desc.where("end_date < ?", Time.zone.now.beginning_of_day) }
