522d42df235e29c0273f2c33f2d896a091d96475
Alan Donohoe <alandonohoe@hotmail.com>
2020-03-23 15:01:23 +0000

    Block a load of scrapers' IP addresses and those requesting ./env
    Security admin.
    
    Keeps causing app errors in rollbar.
    
    ref: https://rollbar.com/alandonohoe/MyBookingSherpa/items/6/?item_page=0&#ip-addresses

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,7 +1,7 @@
-# - - - - - -
-# Throttlers:
-
 unless Rails.env.test?
+  # - - - - - -
+  # Throttlers:
+
   # Throttle sign up attempts every 24 hours
   Rack::Attack.throttle("limit sign ups per day", limit: 2, period: 24.hours) do |request|
     request.ip if request.path == "/guides" && request.post?
@@ -23,11 +23,39 @@ unless Rails.env.test?
   # Blockers:
 
   # NB: Requests are blocked if the return value is truthy
-  Rack::Attack.blocklist("block all access to php scrapers") do |request|
-    request.path.end_with?(".php")
+  Rack::Attack.blocklist("block all access to scrapers hitting get /1, etc") do |request|
+    request.path.end_with?("/1", "/.env") && request.get?
   end
 
-  Rack::Attack.blocklist("block all access to scrapers hitting get /1") do |request|
-    request.path.end_with?("/1") && request.get?
+  # By IP adress:
+  def self.blocked_ip_addresses_403
+    ["13.237.199.116", "185.234.216.58", "194.53.148.71", "51.77.249.198", "80.82.70.206"] +
+      ["199.229.249.162", "91.132.136.50", "37.235.66.207", "62.177.43.154", "178.95.240.188"] +
+      ["178.176.112.213", "77.109.55.209", "185.46.8.135", "123.138.79.105", "132.145.34.57"] +
+      ["188.40.33.78", "192.99.36.177", "51.158.191.84", "106.59.245.48", "66.249.79.90"]
+  end
+
+  def self.blocked_ip_addresses_404
+    []
+  end
+
+  def self.blocked_ip_addresses_503
+    []
+  end
+
+  Rack::Attack.blocklist("block with 403") do |req|
+    blocked_ip_addresses_403.include?(req.ip)
+  end
+
+  Rack::Attack.blocklist("block with 404") do |req|
+    blocked_ip_addresses_404.include?(req.ip)
+  end
+
+  Rack::Attack.blocklist("block with 404 all access to php, etc scrapers") do |req|
+    req.path =~ /.(php|asd|ashx|asp)/
+  end
+
+  Rack::Attack.blocklist("block with 503") do |req|
+    blocked_ip_addresses_503.include?(req.ip)
   end
 end
